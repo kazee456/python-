9b03937ce777f25b41eb0a69e26799cc1e9784be
collins kazee <collinskazee456@gmail.com>
2023-09-07 20:47:48 +0300

    Add files via upload

---
+++ Madlip game.py
@@ -0,0 +1,10 @@
+colour = input("Enter a colour: ")
+color = input("Enter another colour: ")
+verb = input("Enter pick up line: ")
+
+
+
+print("Roses are "+colour )
+print("violets are " +color)
+print(verb)
+

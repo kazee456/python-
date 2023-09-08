7b70610d1de32a94a2210b4a9965229f40931292
collins kazee <collinskazee456@gmail.com>
2023-09-09 00:32:45 +0300

    Add files via upload

---
+++ Dictionaries.py
@@ -0,0 +1,15 @@
+monthcon = {
+    "Jan":"January",
+    "Feb":"February",
+    "Mar":"March",
+    "Apr":"April",
+    "May":"May",
+    "Jun":"June",
+    "Jul":"July",
+    "Aug":"August",
+    "Sep":"September",
+    "Oct":"October",
+    "Nov":"November",
+    "Dec":"December",
+}
+print(monthcon.get("Nov", "not valid"))
